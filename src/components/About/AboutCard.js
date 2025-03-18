9c3c5a0c57101a25965dabe8e6125c671c755c03
Mohammed Bobboi <mbabobboi1@sheffield.ac.uk>
2025-03-18 08:09:06 +0000

    Slightly fixed the about page

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,13 +8,13 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">Mohammed Bobboi </span>
+            from <span className="purple"> Abuja, Nigeria.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I am currently employed as the Head of IT at 180 Degrees Consulting Sheffield.
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            I am currently undergoing an undergraduate degree BSc. in and Computer Science at The
+            University of Sheffield.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
@@ -24,7 +24,7 @@ function AboutCard() {
               <ImPointRight /> Playing Games
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> Playing Football
             </li>
             <li className="about-activity">
               <ImPointRight /> Travelling
@@ -34,7 +34,7 @@ function AboutCard() {
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Mohammed</footer>
         </blockquote>
       </Card.Body>
     </Card>
